7e98c27663baf64b24bcd8979690287bc7f4f21b
[Vasu Navadiya] <[Vasunavadiya923@gmail.com]>
2025-06-25 18:26:43 +0530

    updated code

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def subtraction(a,b,c):
     d = a-b-c
     return a
 
-def multiplication(a,b):
-    return a*b
+def multiplication(a,b,c):
+    return a*b*c
 
 
-def division(a,b):
-    return a/b
+def division(a,b,c):
+    return a/b*c
