bc4132acb82c1a0946422bab4fa8391c4ce0389f
Vasunavadiya90 <vasunavadiya923@gmail.com>
2025-06-25 18:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-def addition(a,b):
-    return a+b
+def addition(a,b,c):
+    return a+b+c
 
-a  = addition(5,3)
+a  = addition(5,3,2)
 print(a)
 
 
-def subtraction(a,b):
-    a = a-b
+def subtraction(a,b,c):
+    d = a-b-c
     return a
 
 def multiplication(a,b):
